0de893949a9fc891916203427dd3d0270a20cfa9
Mateo Centis <mateo.centis@hotmail.com.ar>
2025-03-13 20:38:52 -0300

    Pequeño arreglo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# Import CreateUserView from the API application
+from api.views import CreateUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
